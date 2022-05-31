d6a9684d52ba3cb778d27d3a06c98f5e088de4d9
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-05-31 07:27:22 -0400

    Add postController and authorController

---
+++ controllers/authorController.js
@@ -0,0 +1,7 @@
+const Author = require('../models/author.js');
+
+// Handle Author create on post
+
+exports.author_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
---
+++ controllers/postController.js
@@ -0,0 +1,47 @@
+const Post = require('../models/post.js');
+
+// Display the list of all posts
+exports.post_list = function(req, res) {
+    res.send('Not implemented yet');
+};
+
+// Display the datails of specific post.
+exports.post_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post detail: ' + req.params.id);
+};
+
+// Display Post create form.
+exports.post_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post create GET');
+};
+
+// Handle Post create on POST.
+exports.post_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post create POST');
+};
+
+// Display Post delete form on GET.
+exports.post_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post delete GET');
+};
+
+// Handle Post delete on POST.
+exports.post_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post delete POST');
+};
+
+// Display post update form on GET.
+exports.post_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post update GET');
+};
+
+// Handle post update on POST.
+exports.post_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post update POST');
+};
+
+// Handle post publish on POST.
+exports.post_publish_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Post publish POST');
+};
+
